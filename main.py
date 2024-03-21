558182854e237b5c07d7c7c782bcefb5f935a80d
Paulo Henrique <ph55@hotmail.com>
2024-03-21 19:08:03 -0300

    Update
    Criação da função menu, que será responsável pela interatividade com o usuário

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ import random
 import os
 import time
 
-
+def menu():
+    os.system('cls')
+    print('=======================')
+    print(' WELCOME TO JOKENPÔÔÔ ')
+    print('=======================')
+    list_menu = ['[1] Pedra', '[2] Papel', '[3] Tesoura', '[4] SAIR\n']
+    for x in list_menu:
+        print(x)
