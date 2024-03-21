cdfab367d6d615600ebd6cf924d5dd688f139adf
Paulo Henrique <ph55@hotmail.com>
2024-03-21 19:09:22 -0300

    Update
    Declarando as variáveis que irão dar funcionalidade as placar

--- main.py
+++ main.py
@@ -9,4 +9,8 @@ def menu():
     print('=======================')
     list_menu = ['[1] Pedra', '[2] Papel', '[3] Tesoura', '[4] SAIR\n']
     for x in list_menu:
-        print(x)
+        print(x) 
+
+cont = True
+placar_user = 0
+placar_cpu = 0
